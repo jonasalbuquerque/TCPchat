489353c72e192fa8b859e42a6912994db3efd650
Jonas <jonasalbuquerque@alu.ufc.br>
2021-02-14 20:58:24 -0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/TCPchat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TCPchat.dir/main.cpp.o"
+  "TCPchat"
+  "TCPchat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TCPchat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TCPchat.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TCPchat.
+# This may be replaced when dependencies are built.
