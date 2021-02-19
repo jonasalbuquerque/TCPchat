9d6950d4130724d7378b099c0059a0bb2bd7331c
jonasalbuquerque <jonasalbuquerque@alu.ufc.br>
2021-02-19 17:31:21 -0300

    Add I/O messages

--- sources/main.cpp
+++ sources/main.cpp
@@ -38,17 +38,48 @@ int main()
     try
     {
         std::thread th_server (start_server);
-        std::thread th_client (start_client, 5001);
-//        std::thread th_client2 (start_client, 5002);
-
         th_server.join();
-        th_client.join();
-//        th_client2.join();
-
-        return 0;
+        while (true)
+        {
+            uint8_t counter = 1;
+            std::cout << "Welcome to chat\n" <<
+                      "Enter a command: "    <<
+                      "1. /ENTRAR\n"         <<
+                      "2. /USUARIOS\n"       <<
+                      "3. /SAIR\n"           <<
+                      "or type a message"    << std::endl;
+            std::string entry;
+            std::cin >> entry;
+            if(entry == "/ENTRAR")
+            {
+                std::thread th_client (start_client, 5000 + counter++);
+                th_client.join();
+            }
+            else if (entry == "/USUARIOS")
+            {
+                continue;
+            }
+            else
+            {
+                std::string user;
+                std::cout << "Enter the nickname: ";
+                getline(std::cin, user);
+                //thread = get_thread_by_nickname
+                if (entry == "/SAIR")
+                {
+                    //call_server_to_remove_client(thread)
+                    continue;
+                }
+                else
+                {
+                    //thread.send_to_server(entry)
+                }
+            }
+        }
     }
     catch(const std::exception& e)
     {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
+    return 0;
 }
